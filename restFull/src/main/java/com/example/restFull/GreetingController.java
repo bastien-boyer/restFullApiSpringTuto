5d0cf8f669d97e1470ad058a32b293c42ac66a79
Bast <bastien.boyer.mail@gmail.com>
2024-01-05 16:19:13 +0100

    Ajout commentaire explication annotations

--- restFull/src/main/java/com/example/restFull/GreetingController.java
+++ restFull/src/main/java/com/example/restFull/GreetingController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+
+/*
+This code uses Spring @RestController annotation, which marks the class as a controller where every method returns a domain object instead of a view.
+It is shorthand for including both @Controller and @ResponseBody.
+ */
 @RestController
 public class GreetingController {
 
